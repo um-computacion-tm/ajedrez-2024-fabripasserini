7141b0f1d9d4dd46517c5d484d21614b8b2c2761
fabri <f.passerini@alumno.um.edu.ar>
2024-08-23 11:46:01 -0300

    actualizar carpeta tests con test de tablero

--- tests/test.py
+++ tests/test.py
@@ -17,6 +17,20 @@ class TestTablero(unittest.TestCase):
             self.tablero.mostrar_tablero()
         except Exception as e:
             self.fail(f"mostrar_tablero lanzó una excepción: {e}")
+    
+    def test_mover_pieza_fuera_del_tablero(self):
+        """Prueba que mover una pieza fuera del tablero lanza un ValueError"""
+        with self.assertRaises(ValueError):
+            self.tablero.mover_pieza(-1, 0, 3, 0)  # Coordenada de origen fuera del rango
+
+        with self.assertRaises(ValueError):
+            self.tablero.mover_pieza(1, 0, 8, 0)  # Coordenada de destino fuera del rango
+
+        with self.assertRaises(ValueError):
+            self.tablero.mover_pieza(1, 8, 3, 0)  # Coordenada de origen fuera del rango
+
+        with self.assertRaises(ValueError):
+            self.tablero.mover_pieza(1, 0, 3, 8)
 
 if __name__ == '__main__':
     unittest.main()
