840cc04eca59ce7289cb4edd9c9a9a69f555c657
fabri <f.passerini@alumno.um.edu.ar>
2024-09-05 11:43:58 -0300

    actualizar excepciones

--- chess/excepciones.py
+++ chess/excepciones.py
@@ -1,6 +1,19 @@
 class movimiento_inválido(Exception):
-    pass
+    mensaje = "El movimiento no es válido"
+    def __str__(self):
+        return self
 
 class movimiento_sin_pieza(Exception):
-    pass
+    mensaje = "La posicion se encuentra vacía"
+    def __str__(self):
+        return self
 
+class turno_invalido(Exception):
+    mensaje = "El turno no es válido"
+    def __str__(self):
+        return self
+    
+class fuera_del_tablero(Exception):
+    mensaje = "La posicion indicada se encuentra fuera del tablero"
+    def __str__(self):
+        return self
