3351ccc5e41b3a3a127d70f90f9ed5ccfd871f38
fabri <f.passerini@alumno.um.edu.ar>
2024-09-10 12:57:41 -0300

    actualizar tablero

--- chess/tablero.py
+++ chess/tablero.py
@@ -16,37 +16,60 @@ class Tablero:
                 columna.append(None)
             self.__posiciones.append(columna)
     
-        self.__posiciones[0][0] = Torre("NEGRO") 
-        self.__posiciones[0][7] = Torre("NEGRO") 
-        self.__posiciones[7][7] = Torre("BLANCO") 
-        self.__posiciones[7][0] = Torre("BLANCO")
+        self.__posiciones[0][0] = Torre("NEGRO", self) 
+        self.__posiciones[0][7] = Torre("NEGRO", self) 
+        self.__posiciones[7][7] = Torre("BLANCO", self) 
+        self.__posiciones[7][0] = Torre("BLANCO", self)
 
 
         for i in range(8):
-            self.__posiciones[1][i] = Peon("NEGRO")
-            self.__posiciones[6][i] = Peon("BLANCO")
+            self.__posiciones[1][i] = Peon("NEGRO", self)
+            self.__posiciones[6][i] = Peon("BLANCO", self)
 
     
-        self.__posiciones[0][1] = Caballo("NEGRO")
-        self.__posiciones[0][6] = Caballo("NEGRO")
-        self.__posiciones[7][1] = Caballo("BLANCO")  
-        self.__posiciones[7][6] = Caballo("BLANCO")
+        self.__posiciones[0][1] = Caballo("NEGRO", self)
+        self.__posiciones[0][6] = Caballo("NEGRO", self)
+        self.__posiciones[7][1] = Caballo("BLANCO", self)  
+        self.__posiciones[7][6] = Caballo("BLANCO", self)
 
         
-        self.__posiciones[0][2] = Alfil("NEGRO")  
-        self.__posiciones[0][5] = Alfil("NEGRO")
-        self.__posiciones[7][2] = Alfil("BLANCO")
-        self.__posiciones[7][5] = Alfil("BLANCO")
+        self.__posiciones[0][2] = Alfil("NEGRO", self)  
+        self.__posiciones[0][5] = Alfil("NEGRO", self)
+        self.__posiciones[7][2] = Alfil("BLANCO", self)
+        self.__posiciones[7][5] = Alfil("BLANCO", self)
 
         
-        self.__posiciones[0][3] = Dama("NEGRO")
-        self.__posiciones[7][3] = Dama("BLANCO")
+        self.__posiciones[0][3] = Dama("NEGRO", self)
+        self.__posiciones[7][3] = Dama("BLANCO", self)
 
-        self.__posiciones[0][4] = Rey("NEGRO")
-        self.__posiciones[7][4] = Rey("BLANCO")
+        self.__posiciones[0][4] = Rey("NEGRO", self)
+        self.__posiciones[7][4] = Rey("BLANCO", self)
+    
+    def __str__(self):
+        tablero_str = ""
+        for fila in self.__posiciones:
+            for cell in fila:
+                if cell is not None:
+                    tablero_str += str(cell)
+                else:
+                    tablero_str += " "
+            tablero_str += "\n"
+        return tablero_str
+    
+    
+    def mostrar_tablero(self):
+        for fila in self.__posiciones:
+            fila_str = ""
+            for pieza in fila:
+                if pieza is None:
+                    fila_str += ". "
+                else:
+                    fila_str += str(pieza) + " "
+            print(fila_str)
 
 if __name__ == "__main__":
     tablero = Tablero()
+    tablero.mostrar_tablero()
     
 
     
