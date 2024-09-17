9ba927b17003fd4b10a318657972b94bd1be4a7c
fabri <f.passerini@alumno.um.edu.ar>
2024-09-17 17:46:08 -0300

    añadir posibles posiciones a peon

--- chess/piezas/peon.py
+++ chess/piezas/peon.py
@@ -1,8 +1,17 @@
 from chess.piezas.pieza import Pieza
 
+
 class Peon(Pieza):
     blanco_str = "♟"
     negro_str = "♙"
 
-    def movimientos_validos(self):
-        pass
+    def obtener_posibles_posiciones(self, desde_fila, desde_columna):
+       pass
+
+    def obtener_posibles_comer(self, desde_fila, dese_columna):
+        if self.__color__ == "NEGRO":
+            otra_pieza = self.__tablero__.obtener_pieza(desde_fila + 1, dese_columna + 1)
+            if otra_pieza and otra_pieza.__color__ == "BLANCO":
+                return [(desde_fila + 1, dese_columna + 1)]
+
+        return []
