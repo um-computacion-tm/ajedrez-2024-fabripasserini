889e47546f66c84c76a59ac892450364287f90b1
fabri <f.passerini@alumno.um.edu.ar>
2024-09-18 19:48:31 -0300

    añadir posibles posiciones mover a peon

--- chess/piezas/peon.py
+++ chess/piezas/peon.py
@@ -6,7 +6,11 @@ class Peon(Pieza):
     negro_str = "♙"
 
     def obtener_posibles_posiciones(self, desde_fila, desde_columna):
-       pass
+        posibles = self.obtener_posiciones_mover(desde_fila, desde_columna)
+        posibles.extend(
+            self.obtener_posibles_comer(desde_fila, desde_columna)
+        )
+        return posibles
 
     def obtener_posibles_comer(self, desde_fila, dese_columna):
         if self.__color__ == "NEGRO":
@@ -14,4 +18,34 @@ class Peon(Pieza):
             if otra_pieza and otra_pieza.__color__ == "BLANCO":
                 return [(desde_fila + 1, dese_columna + 1)]
 
+        return []
+    
+    def obtener_posiciones_mover(self, desde_fila, desde_columna):
+        if self.__color__ == "NEGRO":
+            if self.__tablero__.obtener_pieza(desde_fila + 1, desde_columna) is None:
+                if (
+                    desde_fila == 1 and
+                    self.__tablero__.obtener_pieza(desde_fila + 2, desde_columna) is None
+                ):
+                    return [
+                        (desde_fila + 1, desde_columna),
+                        (desde_fila + 2, desde_columna)
+                    ]
+                else:
+                    return [
+                        (desde_fila + 1, desde_columna),
+                    ]
+        else:
+            if desde_fila == 6:
+                return [
+                    (desde_fila - 1, desde_columna),
+                    (desde_fila - 2, desde_columna)
+                ]
+            else:
+                if self.__tablero__.obtener_pieza(desde_fila - 1, desde_columna) is None:
+                    return [
+                        (desde_fila - 1, desde_columna),
+                    ]
+                else:
+                    return []
         return []
