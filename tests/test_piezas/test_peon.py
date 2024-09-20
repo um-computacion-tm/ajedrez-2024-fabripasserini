4394ff4a6c1f1e1d150bb69bae482c81854d3f02
fabri <f.passerini@alumno.um.edu.ar>
2024-09-20 14:59:29 -0300

    añadir test de peon

--- tests/test_piezas/test_peon.py
+++ tests/test_piezas/test_peon.py
@@ -17,9 +17,9 @@ class TestPeon(unittest.TestCase):
         tablero = Tablero()
         peon = Peon("NEGRO", tablero)
 
-        possibles = peon.obtener_posibles_posiciones(1, 5)
+        posibles = peon.obtener_posibles_posiciones(1, 5)
         self.assertEqual(
-            possibles,
+            posibles,
             [(2, 5), (3, 5)]
         )
 
@@ -27,9 +27,9 @@ class TestPeon(unittest.TestCase):
         tablero = Tablero()
         peon = Peon("NEGRO", tablero)
 
-        possibles = peon.obtener_posibles_posiciones(2, 5)
+        posibles = peon.obtener_posibles_posiciones(2, 5)
         self.assertEqual(
-            possibles,
+            posibles,
             [(3, 5)]
         )
 
@@ -38,12 +38,43 @@ class TestPeon(unittest.TestCase):
         tablero = Tablero()
         peon = Peon("BLANCO", tablero)
 
-        possibles = peon.obtener_posibles_posiciones(6, 4)
+        posibles = peon.obtener_posibles_posiciones(6, 4)
         self.assertEqual(
-            possibles,
+            posibles,
             [(5, 4), (4, 4)]
         )
 
+    def test_not_initial_white(self):
+        tablero = Tablero()
+        peon = Peon("BLANCO", tablero)
+
+        posibles = peon.obtener_posibles_posiciones(5, 4)
+        self.assertEqual(
+            posibles,
+            [(4, 4)]
+        )
+
+    def test_not_initial_white_block(self):
+        tablero = Tablero()
+        peon = Peon("BLANCO", tablero)
+        tablero.poner_pieza(4, 4, Peon("NEGRO", tablero))
+
+        posibles = peon.obtener_posibles_posiciones(5, 4)
+        self.assertEqual(
+            posibles,
+            []
+        )
+
+    def test_not_initial_black_block(self):
+        tablero = Tablero()
+        peon = Peon("NEGRO", tablero)
+        tablero.poner_pieza(5, 4, Peon("NEGRO", tablero))
+
+        posibles = peon.obtener_posibles_posiciones(4, 4)
+        self.assertEqual(
+            posibles,
+            []
+        )
     
 
 if __name__ == '__main__':
