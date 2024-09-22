d95867801fd823c7628bedc46a84ee3fa33e5518
fabri <f.passerini@alumno.um.edu.ar>
2024-09-22 19:48:02 -0300

    añadir tests de posibles posiciones a torre

--- tests/test_piezas/test_torre.py
+++ tests/test_piezas/test_torre.py
@@ -23,17 +23,47 @@ class TestTorre(unittest.TestCase):
             [(3, 1), (2, 1), (1, 1), (0, 1)]
         )
 
-    def test_move_vertical_desc_with_own_piece(self):
+   # def test_mover_vertical_desc(self):
+   #     tablero = Tablero()
+   #     torre = Torre("BLANCO", tablero)
+   #     posibles = torre.posibles_posiciones_vd(4, 1)
+   #     self.assertEqual(
+   #         posibles,
+   #         [(5, 1), (6, 1), (7, 1)]
+   #     )
+    
+    def test_mover_vertical_desc_con_pieza_propia(self):
         tablero = Tablero()
         tablero.poner_pieza(6, 1, Peon("BLANCO", tablero))
         torre = Torre("BLANCO", tablero)
         tablero.poner_pieza(4, 1, torre)
-        possibles = torre.posibles_posiciones_vd(4, 1)
+        posibles = torre.posibles_posiciones_vd(4, 1)
         self.assertEqual(
-            possibles,
+            posibles,
             [(5, 1)]
         )
+    def test_mover_vertical_desc_con_pieza_contraria(self):
+        tablero = Tablero()
+        tablero.poner_pieza(6, 1, Peon("NEGRO", tablero))
+        torre = Torre("BLANCO", tablero)
+        tablero.poner_pieza(4, 1, torre)
+        posibles = torre.posibles_posiciones_vd(4, 1)
+        self.assertEqual(
+            posibles,
+            [(5, 1), (6, 1)]
+        )
 
+    def test_mover_diagonal_desc(self):
+        tablero = Tablero()
+        torre = tablero.obtener_pieza(columna=0, fila=0)
+        es_posible = torre.posiciones_validas(
+            desde_fila=0,
+            desde_columna=0,
+            hasta_fila=1,
+            hasta_columna=1,
+        )
+
+        self.assertFalse(es_posible)
    
         
 if __name__ == '__main__':
