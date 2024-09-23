8d5c585007af3fa885fb46afdb217403d00d4a34
fabri <f.passerini@alumno.um.edu.ar>
2024-09-23 13:28:03 -0300

    agregar test de pieza

--- tests/test_piezas/testpieza.py
+++ tests/test_piezas/testpieza.py
@@ -1,10 +1,13 @@
 import unittest
 from chess.tablero import Tablero
 from chess.piezas.pieza import Pieza
+from chess.piezas.torre import Torre
 
 class TestPieza(unittest.TestCase):
     def setUp(self):
         self.tablero_real = Tablero()
+        self.pieza_blanca = Pieza("BLANCO", self.tablero_real)
+        self.pieza_negra = Pieza("NEGRO", self.tablero_real)
 
     def test_str_blanco(self):
         Pieza.blanco_str = "♜"
@@ -18,6 +21,31 @@ class TestPieza(unittest.TestCase):
         pieza_negra = Pieza("NEGRO", self.tablero_real)
         self.assertEqual(str(pieza_negra), "♖")
 
+    def test_obtener_color(self):
+        self.assertEqual(self.pieza_blanca.obtener_color(), "BLANCO")
+        self.assertEqual(self.pieza_negra.obtener_color(), "NEGRO")
+
+    def test_posibles_posiciones_vd_sin_bloqueo(self):
+        posiciones = self.pieza_blanca.posibles_posiciones_vd(3, 3)
+        esperado = [(4, 3), (5, 3)]
+        self.assertEqual(posiciones, esperado)
+
+    def test_mover(self):
+        tablero = Tablero()
+        torre = Torre(color='NEGRO', tablero=tablero)
+        tablero.poner_pieza(0, 0, torre)
+
+        tablero.mover(
+            desde_fila=0,
+            desde_columna=0,
+            hasta_fila=0,
+            hasta_columna=1,
+        )
+
+        self.assertIsInstance(
+            tablero.obtener_pieza(0, 1),
+            Torre,
+        )
 
 if __name__ == '__main__':
     unittest.main()
