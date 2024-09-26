4e85fa70b6473d0baeae1996285fe3d0f4d99fd0
fabri <f.passerini@alumno.um.edu.ar>
2024-09-26 13:10:19 -0300

    agregar posibles posiciones asc a alfil

--- chess/piezas/alfil.py
+++ chess/piezas/alfil.py
@@ -6,14 +6,14 @@ class Alfil(Pieza):
     
     def posiciones_validas(self, desde_fila, desde_columna, hasta_fila, hasta_columna):
         posible_posiciones = (
-        self.posibles_posiciones_diagonal_sd(desde_fila, desde_columna) +
-            self.posibles_posiciones_diagonal_si(desde_fila, desde_columna) +
+        self.posibles_posiciones_diagonal_dd(desde_fila, desde_columna) +
             self.posibles_posiciones_diagonal_id(desde_fila, desde_columna) +
-            self.posibles_posiciones_diagonal_ii(desde_fila, desde_columna)
+            self.posibles_posiciones_diagonal_ia(desde_fila, desde_columna) +
+            self.posibles_posiciones_diagonal_da(desde_fila, desde_columna)
         )
         return (hasta_fila, hasta_columna) in posible_posiciones
 
-    def posibles_posiciones_diagonal_sd(self, fila, columna):
+    def posibles_posiciones_diagonal_dd(self, fila, columna):
         posibles = []
         siguiente_fila, siguiente_columna = fila + 1, columna + 1
         while siguiente_fila < 8 and siguiente_columna < 8:
@@ -27,7 +27,7 @@ class Alfil(Pieza):
             siguiente_columna += 1
         return posibles
 
-    def posibles_posiciones_diagonal_si(self, fila, columna):
+    def posibles_posiciones_diagonal_id(self, fila, columna):
         posibles = []
         siguiente_fila, siguiente_columna = fila + 1, columna - 1
         while siguiente_fila < 8 and siguiente_columna >= 0:
@@ -38,5 +38,33 @@ class Alfil(Pieza):
                 break
             posibles.append((siguiente_fila, siguiente_columna))
             siguiente_fila += 1
+            siguiente_columna -= 1
+        return posibles
+
+    def posibles_posiciones_diagonal_da(self, fila, columna):
+        posibles = []
+        siguiente_fila, siguiente_columna = fila - 1, columna + 1
+        while siguiente_fila >= 0 and siguiente_columna < 8:
+            otra_pieza = self.__tablero__.obtener_pieza(siguiente_fila, siguiente_columna)
+            if otra_pieza is not None:
+                if otra_pieza.__color__ != self.__color__:
+                    posibles.append((siguiente_fila, siguiente_columna))
+                break
+            posibles.append((siguiente_fila, siguiente_columna))
+            siguiente_fila -= 1
+            siguiente_columna += 1
+        return posibles
+
+    def posibles_posiciones_diagonal_ia(self, fila, columna):
+        posibles = []
+        siguiente_fila, siguiente_columna = fila - 1, columna - 1
+        while siguiente_fila >= 0 and siguiente_columna >= 0:
+            otra_pieza = self.__tablero__.obtener_pieza(siguiente_fila, siguiente_columna)
+            if otra_pieza is not None:
+                if otra_pieza.__color__ != self.__color__:
+                    posibles.append((siguiente_fila, siguiente_columna))
+                break
+            posibles.append((siguiente_fila, siguiente_columna))
+            siguiente_fila -= 1
             siguiente_columna -= 1
         return posibles
