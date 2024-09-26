97a1d5a45890633ccb52a65698824e67a2b28ef4
fabri <f.passerini@alumno.um.edu.ar>
2024-09-25 22:59:01 -0300

    agregar posibles posiciones desc a alfil

--- chess/piezas/alfil.py
+++ chess/piezas/alfil.py
@@ -4,7 +4,39 @@ class Alfil(Pieza):
     blanco_str = "♝"
     negro_str = "♗"
     
-    
+    def posiciones_validas(self, desde_fila, desde_columna, hasta_fila, hasta_columna):
+        posible_posiciones = (
+        self.posibles_posiciones_diagonal_sd(desde_fila, desde_columna) +
+            self.posibles_posiciones_diagonal_si(desde_fila, desde_columna) +
+            self.posibles_posiciones_diagonal_id(desde_fila, desde_columna) +
+            self.posibles_posiciones_diagonal_ii(desde_fila, desde_columna)
+        )
+        return (hasta_fila, hasta_columna) in posible_posiciones
 
-    def movimientos_validos(self):
-        pass
+    def posibles_posiciones_diagonal_sd(self, fila, columna):
+        posibles = []
+        siguiente_fila, siguiente_columna = fila + 1, columna + 1
+        while siguiente_fila < 8 and siguiente_columna < 8:
+            otra_pieza = self.__tablero__.obtener_pieza(siguiente_fila, siguiente_columna)
+            if otra_pieza is not None:
+                if otra_pieza.__color__ != self.__color__:
+                    posibles.append((siguiente_fila, siguiente_columna))
+                break
+            posibles.append((siguiente_fila, siguiente_columna))
+            siguiente_fila += 1
+            siguiente_columna += 1
+        return posibles
+
+    def posibles_posiciones_diagonal_si(self, fila, columna):
+        posibles = []
+        siguiente_fila, siguiente_columna = fila + 1, columna - 1
+        while siguiente_fila < 8 and siguiente_columna >= 0:
+            otra_pieza = self.__tablero__.obtener_pieza(siguiente_fila, siguiente_columna)
+            if otra_pieza is not None:
+                if otra_pieza.__color__ != self.__color__:
+                    posibles.append((siguiente_fila, siguiente_columna))
+                break
+            posibles.append((siguiente_fila, siguiente_columna))
+            siguiente_fila += 1
+            siguiente_columna -= 1
+        return posibles
