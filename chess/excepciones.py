1120432b3fd89901179d4ebda4570b62ee201275
fabri <f.passerini@alumno.um.edu.ar>
2024-10-09 23:25:47 -0300

    arreglar excepciones

--- chess/excepciones.py
+++ chess/excepciones.py
@@ -1,31 +1,31 @@
 class movimiento_inválido(Exception):
-    def __init__(self, mensaje):
+    def __init__(self, mensaje="El movimiento no es válido"):
         super().__init__(mensaje)
-        self.__mensaje__ = "El movimiento no es válido"
+        self.__mensaje__ = mensaje
 
     def __str__(self):
         return self.__mensaje__
 
 class turno_invalido(Exception):
-    def __init__(self, mensaje):
+    def __init__(self, mensaje="El turno no es válido"):
         super().__init__(mensaje)
-        self.__mensaje__ = "El turno no es válido"
+        self.__mensaje__ = mensaje
 
     def __str__(self):
         return self.__mensaje__
-    
+
 class fuera_del_tablero(Exception):
-    def __init__(self, mensaje):
+    def __init__(self, mensaje="La posicion indicada se encuentra fuera del tablero"):
         super().__init__(mensaje)
-        self.__mensaje__ = "La posicion indicada se encuentra fuera del tablero"
+        self.__mensaje__ = mensaje
 
     def __str__(self):
         return self.__mensaje__
 
 class movimiento_sin_pieza(Exception):
-    def __init__(self, mensaje):
+    def __init__(self, mensaje="Movimiento sin pieza"):
         super().__init__(mensaje)
-        self.__mensaje__ = "Movimiento sin pieza"
+        self.__mensaje__ = mensaje
 
     def __str__(self):
         return self.__mensaje__
