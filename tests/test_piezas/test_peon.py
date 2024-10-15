abd377d0720fa3581d8b9c87d64f1713d1494b9e
fabri <f.passerini@alumno.um.edu.ar>
2024-10-14 23:59:49 -0300

    arreglar tests peon

--- tests/test_piezas/test_peon.py
+++ tests/test_piezas/test_peon.py
@@ -13,14 +13,10 @@ class TestPeon(unittest.TestCase):
             "♟",
         )
 
-
-
-class TestPeon(unittest.TestCase):
-
     def setUp(self):
         self.tablero = Tablero()
         self.peon_blanco = Peon("BLANCO", self.tablero)
-        self.peon_negro = Peon("NEGRO", self.tablero)
+        self.peon_negro1 = Peon("NEGRO", self.tablero)
 
     def test_obtener_posicion_simple_blanco(self):
         self.tablero.poner_pieza(6, 4, self.peon_blanco)
@@ -28,8 +24,8 @@ class TestPeon(unittest.TestCase):
         self.assertIn((5, 4), posiciones)
 
     def test_obtener_posicion_simple_negro(self):
-        self.tablero.poner_pieza(1, 4, self.peon_negro)
-        posiciones = self.peon_negro.obtener_posicion_simple(1, 4, 1)
+        self.tablero.poner_pieza(1, 4, self.peon_negro1)
+        posiciones = self.peon_negro1.obtener_posicion_simple(1, 4, 1)
         self.assertIn((2, 4), posiciones)
     
     def test_obtener_posicion_inicial_doble_blanco(self):
@@ -38,8 +34,8 @@ class TestPeon(unittest.TestCase):
         self.assertIn((4, 4), posiciones)
 
     def test_obtener_posicion_inicial_doble_negro(self):
-        self.tablero.poner_pieza(1, 4, self.peon_negro)
-        posiciones = self.peon_negro.obtener_posicion_inicial_doble(1, 4, 1, 1)
+        self.tablero.poner_pieza(1, 4, self.peon_negro1)
+        posiciones = self.peon_negro1.obtener_posicion_inicial_doble(1, 4, 1, 1)
         self.assertIn((3, 4), posiciones)
     
     def test_movimiento_bloqueado(self):
@@ -49,6 +45,7 @@ class TestPeon(unittest.TestCase):
         with self.assertRaises(movimiento_inválido):
             self.peon_blanco.obtener_posicion_simple(6, 4, -1)
 
+    
     def test_fuera_del_tablero(self):
         with self.assertRaises(fuera_del_tablero):
             self.peon_blanco.es_posicion_valida(8, 4)
@@ -67,8 +64,8 @@ class TestPeon(unittest.TestCase):
         self.assertIn((4, 4), posiciones)
 
     def test_obtener_posiciones_mover_negro(self):
-        self.tablero.poner_pieza(1, 4, self.peon_negro)
-        posiciones = self.peon_negro.obtener_posiciones_mover(1, 4)
+        self.tablero.poner_pieza(1, 4, self.peon_negro1)
+        posiciones = self.peon_negro1.obtener_posiciones_mover(1, 4)
         self.assertIn((2, 4), posiciones)
         self.assertIn((3, 4), posiciones)
     
@@ -80,8 +77,8 @@ class TestPeon(unittest.TestCase):
 
     
     def test_peon_negro_esquina_inferior_izquierda(self):
-        self.tablero.poner_pieza(1, 0, self.peon_negro)
-        posiciones = self.peon_negro.obtener_posiciones_mover(1, 0)
+        self.tablero.poner_pieza(1, 0, self.peon_negro1)
+        posiciones = self.peon_negro1.obtener_posiciones_mover(1, 0)
         self.assertIn((2, 0), posiciones)  
         self.assertIn((3, 0), posiciones)  
 
@@ -93,10 +90,12 @@ class TestPeon(unittest.TestCase):
         self.assertIn((4, 7), posiciones)  
 
     def test_peon_negro_esquina_inferior_derecha(self):
-        self.tablero.poner_pieza(1, 7, self.peon_negro)
-        posiciones = self.peon_negro.obtener_posiciones_mover(1, 7)
+        self.tablero.poner_pieza(1, 7, self.peon_negro1)
+        posiciones = self.peon_negro1.obtener_posiciones_mover(1, 7)
         self.assertIn((2, 7), posiciones)  
         self.assertIn((3, 7), posiciones)
 
+
+    
 if __name__ == '__main__':
     unittest.main()
